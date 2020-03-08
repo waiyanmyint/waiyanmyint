5089770b82d4aa66edb684799681532124698e6d
Wai Yan Myint <myint.waiyan1996@gmail.com>
2020-03-08 12:14:29 +0630

    If_Else_Statement

---
+++ If_Else.py
@@ -0,0 +1,13 @@
+x = 50
+
+if x >10:
+    print("x is ten")
+    if x > 20:
+        print("x is greater than 20")
+    else:
+        print("No, x is not greater than 20")
+
+if x > 10 and x != 10 or x > 20:
+    print("x is greater than 10 and 20")
+else:
+    print("x is not greater than 10 & 20")
---
+++ If_Else_Statement.py
@@ -0,0 +1,37 @@
+#If Else Statements
+
+
+#Boolean Expression
+
+print (20 > 10)
+print (20 == 10)
+print (20 < 10)
+print (bool("Hello World"))
+print (bool(20))
+
+
+Python Conditions
+
+Equals						-> x == y
+Not Equals					-> x != y
+Less Than					-> x <  y
+Less Than or Equals 		-> x <= y
+Greater Than 				-> x >  y
+Greater Than or Equals 		-> x >= y
+Boolean OR 					-> x or y , x | y
+Boolean AND 				-> x and y, x & y
+Boolean NOT 				-> not x
+
+
+if -
+else 
+
+
+#If statement
+
+x = 60
+y = 7
+
+if x > y:
+	print ("x is greater than y")
+
